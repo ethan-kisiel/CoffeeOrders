ccd48cd26bb1ad81bbabdf81fb3b100d786a6c01
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-10-03 13:00:48 -0400

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Ethan Kisiel on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
